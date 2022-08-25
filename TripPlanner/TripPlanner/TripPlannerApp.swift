a5075cdf60a7c904c604cf865a487d18afc93858
Alexander Takacs <takacs@cngroup.dk>
2022-08-25 11:09:52 +0200

    create project and add swift-format

---
+++ TripPlanner/TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Alexander Takács on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
